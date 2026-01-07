5c6abf234c5daa43468032de0132ac83a97d6792
Faudil Puttilli <faudil.puttilli@wiseone.io>
2026-01-07 02:23:53 +0100

    I've implemented power instead of sleeping

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # eval-expr
 A simple rust eval expr I developed quickly for fun
 
-I might add parenthesis or other operation later
+I might add parenthesis or other operations later
--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,19 @@ fn subtract(buffer: &mut Peekable<Chars>, value: i32) -> i32 {
 }
 
 fn multiply(buffer: &mut Peekable<Chars>, value: i32) -> i32 {
-    let factor = value * parse_number(buffer);
+    let factor = value * pow_eval_expr(buffer);
     eval_expr(buffer, factor, true)
 }
 
 fn divide(buffer: &mut Peekable<Chars>, value: i32) -> i32 {
-    let quotien = value / parse_number(buffer);
-    eval_expr(buffer, quotien, true)
+    let quotient = value / pow_eval_expr(buffer);
+    eval_expr(buffer, quotient, true)
 }
 
+fn power(buffer: &mut Peekable<Chars>, value: i32) -> i32 {
+    let pow = i32::pow(value, parse_number(buffer) as u32);
+    eval_expr(buffer, pow, true)
+}
 
 fn parse_number(buffer: &mut Peekable<Chars>) -> i32 {
     let mut is_number = true;
@@ -40,12 +44,21 @@ fn parse_number(buffer: &mut Peekable<Chars>) -> i32 {
             number_str = number_str + token_char.to_string().as_str(); // Yeah yeah ugly I know
             buffer.next();
         } else { is_number = false; }
-
     }
     if number_str.is_empty() {
         return 0;
     }
     number_str.parse::<i32>().unwrap() * factor
+}
+
+fn pow_eval_expr(buffer: &mut Peekable<Chars>) -> i32 {
+    let value = parse_number(buffer);
+    let token = buffer.next();
+    match token {
+        Some('^') => {power(buffer, value)},
+        None => {value}
+        Some(_) => {value}
+    }
 }
 
 fn eval_expr(buffer: &mut Peekable<Chars>, left_value: i32, has_left_value: bool) -> i32 {
@@ -56,6 +69,7 @@ fn eval_expr(buffer: &mut Peekable<Chars>, left_value: i32, has_left_value: bool
         Some('-') => {subtract(buffer, value)},
         Some('*') => {multiply(buffer, value)},
         Some('/') => {divide(buffer, value)},
+        Some('^') => {power(buffer, value)},
         None => {value}
         Some(_) => {value}
     }
@@ -63,6 +77,10 @@ fn eval_expr(buffer: &mut Peekable<Chars>, left_value: i32, has_left_value: bool
 
 
 fn main() {
+    let expr = "3*2^3".to_string().replace(" ", "");
+    let mut expr_peekable = expr.chars().peekable();
+    println!("{}={}", expr, eval_expr(& mut expr_peekable, 0, false));
+
     for argument in env::args() {
         let expr = argument.replace(" ", "");
         let mut expr_peekable = expr.chars().peekable();
